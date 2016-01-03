e2404d94ed6d26a18d2444c295382325b6c9ef45
Mikolaj Magnuski <mmagnuski@swps.edu.pl>
2016-01-04 00:49:37 +0100

    fix keyword args

--- src/windows.jl
+++ src/windows.jl
@@ -14,8 +14,7 @@ type sample_window <: Window
 	step::Int64
 end
 
-function tosamples(w::time_window,
-	sf=1000::Union(AbstractFloat, Int))
+function tosamples(w::time_window; sf::Union(AbstractFloat, Int)=1000)
 
 	return sample_window(round(Int, w.start * sf),
 		round(Int, w.width * sf), round(Int, w.step * sf))
@@ -26,8 +25,8 @@ function +(w::Window, val::Integer)
 	return w
 end
 
-function win(width::AbstractFloat, step::AbstractFloat,
-	sf=1000::Union(AbstractFloat, Int))
+function win(width::AbstractFloat, step::AbstractFloat;
+	sf::Union(AbstractFloat, Int)=1000)
 
 	w = tosamples(time_window(0., width, step), sf)
 	w.start = 1
