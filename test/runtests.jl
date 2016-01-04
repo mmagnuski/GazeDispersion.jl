81504145840dc5c8cd86559d7bc891d66fc08ec0
mmagnuski <mmagnuski@swps.edu.pl>
2016-01-04 21:41:13 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,9 @@ using Base.Test
 using GazeDispersion
 
 et_small = [0.1 0.1; 0.2 0.2]
-@test GazeDispersion.all_euclidist(et_small, et_small) == (2 * 0.1414213562373095)/4
+@test_approx_eq GazeDispersion.all_euclidist(et_small, et_small) (2 * 0.1414213562373095)/4
 
 # window tests
-@test win(0.25, 0.05, sf=1000) == GazeDispersion.sample_window(1, 250, 50)
+w1 = win(0.25, 0.05, sf=1000)
+w2 = GazeDispersion.sample_window(1, 250, 50)
+@test all([w1.start, w1.step, w1.width] .== [w2.start, w2.step, w2.width])
