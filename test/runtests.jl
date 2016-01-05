b509e5c3fd71c36a218d14e8e8eec7a329a939c8
mmagnuski <mmagnuski@swps.edu.pl>
2016-01-05 01:34:11 +0100

    test adding value to window

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,3 +8,6 @@ et_small = [0.1 0.1; 0.2 0.2]
 w1 = win(0.25, 0.05, sf=1000)
 w2 = GazeDispersion.sample_window(1, 250, 50)
 @test all([w1.start, w1.step, w1.width] .== [w2.start, w2.step, w2.width])
+
+w2 + 1
+@test w2.start == 51
