a13aefb29ad844c420d3d646decd29edb604b07d
mmagnuski <mmagnuski@swps.edu.pl>
2016-01-15 17:38:24 +0100

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,11 @@ w1 = win(0.25, 0.05, sf=1000)
 w2 = GazeDispersion.sample_window(1, 250, 50)
 @test all([w1.start, w1.step, w1.width] .== [w2.start, w2.step, w2.width])
 
-w2 + 1
-@test w2.start == 51
+@test (w2 + 1).start == 51
+
+w1 = GazeDispersion.sample_window(1, 2, 1)
+mat = map(Float64, repmat(collect(1:4)', 3, 1))
+dat = GazeDispersion.get_data(mat, w1)
+@test all(dat .== map(Float64, [1 2; 1 2; 3 4; 3 4]))
+
+@test GazeDispersion.window_steps(mat, w1) == 2
